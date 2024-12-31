5cecfcb8df0298fa55e78b12f58427078c445243
HandeMalkocoglu <handemalkocoglu@hotmail.com>
2024-12-31 17:02:29 +0300

    Success sayfası iterasyon 1 e göre tamamlandı

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
+import React from 'react';
 import "../../images/iteration-1-images/logo.svg";
 import "./Success.css";
 
-function Success({onBack}) {
+function Success({ onBack }) {
   return (
     <div className='success' data-cy="success">
-        <div className='logo'>
-            <img src="../../images/iteration-1-images/logo.svg"/>
-        </div>
-        <div className='success-message'>
-            <p data-cy="success-congratulations">TEBRİKLER!</p>
-            <p data-cy="success-order-received">SİPARİŞİNİZ ALINDI!</p>
-        </div>
-        <div className="success-button">
-            <button onClick={onBack} data-cy="back-to-home">Anasayfaya Dön</button>
-        </div>
+      <div className='logo'>
+        <img src="../../images/iteration-1-images/logo.svg" alt="Logo" />
+      </div>
+      <div className='success-message'>
+        <p data-cy="success-congratulations">TEBRİKLER!</p>
+        <p data-cy="success-order-received">SİPARİŞİNİZ ALINDI!</p>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Success
+export default Success;
