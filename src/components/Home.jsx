9a4e1ff6c03ef8ee6b662325d4d76b4ed2e7907f
HandeMalkocoglu <handemalkocoglu@hotmail.com>
2025-01-06 02:10:53 +0300

    Router yöntemine geçiş yapıldı Success compenenti için iterasyon2 güncellenmesi yapıldı

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import "./Home.css"
-import '../../images/iteration-1-images/home-banner.png'
+import { Link } from "react-router-dom";
+import "./Home.css";
 
-export default function Home ({onButtonClick}) {
-return (
+export default function Home() {
+  return (
     <div className="home-page" data-cy="home-page">
-        <img src="../../images/iteration-1-images/logo.svg" alt="" />
-        <p>KOD ACIKTIRIR <br/>
-            PİZZA, DOYURUR
-        </p>
-        <button data-cy="home-button" onClick={onButtonClick} className="button" >ACIKTIM</button>
+      <img src="../../images/iteration-1-images/logo.svg" alt="" />
+      <p>
+        KOD ACIKTIRIR <br />
+        PİZZA, DOYURUR
+      </p>
+      <Link to="/order">
+        <button data-cy="home-button" className="button">
+          ACIKTIM
+        </button>
+      </Link>
     </div>
-)
+  );
 }
